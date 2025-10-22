5a58046506d8d049b8c7eccaf32f8bc2b43ae115
Andrei Yeudakimau <andrei.yeudakimau@student.ehu.lt>
2025-10-22 20:08:32 +0300

    chore: clear main branch

--- apps/log-analyzer/index.js
+++
@@ -1,2 +0,0 @@
-﻿#!/usr/bin/env node
-console.log('Log Analyzer: каркас готов (парсинг и CLI-фильтры добавим на шаге 3).');
--- apps/log-generator/index.js
+++
@@ -1,4 +0,0 @@
-﻿#!/usr/bin/env node
-import Logger from '../../shared/logger.js';
-const logger = new Logger();
-console.log('Log Generator: каркас готов (реализацию таймеров добавим на шаге 2).');
