e3f1adace43aabdac899c30b3f60cc6749144723
Andrei Yeudakimau <andrei.yeudakimau@student.ehu.lt>
2025-10-22 17:27:31 +0300

    init: scaffold (apps + shared logger stub, scripts, .gitignore)

---
+++ apps/log-analyzer/index.js
@@ -0,0 +1,2 @@
+﻿#!/usr/bin/env node
+console.log('Log Analyzer: каркас готов (парсинг и CLI-фильтры добавим на шаге 3).');
---
+++ shared/logger.js
@@ -0,0 +1,10 @@
+﻿/**
+ * Общий класс логгера (заглушка).
+ * Далее расширим: расчёт путей по минутам, формат JSON-строки и запись в файл.
+ */
+export default class Logger {
+  constructor(baseDir = 'logs') {
+    this.baseDir = baseDir;
+  }
+  // В следующих шагах добавим: getCurrentMinuteDir(), format(), append()
+}
