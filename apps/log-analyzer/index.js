2b7b0f494229c02890bd432b8e4788a75412e21c
Andrei Yeudakimau <andrei.yeudakimau@student.ehu.lt>
2025-11-03 00:04:18 +0200

    feat:add solution

--- apps/log-analyzer/index.js
+++ apps/log-analyzer/index.js
@@ -1,2 +1,1 @@
 ﻿#!/usr/bin/env node
-console.log('Log Analyzer: каркас готов (парсинг и CLI-фильтры добавим на шаге 3).');
--- apps/log-generator/index.js
+++ apps/log-generator/index.js
@@ -1,4 +1,4 @@
 ﻿#!/usr/bin/env node
 import Logger from '../../shared/logger.js';
 const logger = new Logger();
-console.log('Log Generator: каркас готов (реализацию таймеров добавим на шаге 2).');
+
